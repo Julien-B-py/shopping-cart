f040922b160b47db09b2ade34cd2e3e56ddf8445
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2022-04-28 23:39:59 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panier</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+
+
+    <div class="basket__items">
+
+    <h1>Mon panier</h1>
+
+        <div class="basket__item">
+
+            <img src="01.jpg" alt="">
+
+            <div class="basket__item__right">
+                <div class="basket__item__right__top">
+                    <div class="basket__item__text">
+                        Carte mère MSI B550-A Pro ATX - Socket AM4
+                    </div>
+                    <div class="basket__item__price">
+                        101.99€
+                    </div>
+                </div>
+
+
+                <div class="basket__item__quantity">
+                    2
+                </div>
+
+                <div class="basket__item__total">
+
+                </div>
+            </div>
+
+        </div>
+
+        <div class="basket__item">
+
+            <img src="02.jpg" alt="">
+
+            <div class="basket__item__right">
+
+                <div class="basket__item__right__top">
+                    <div class="basket__item__text">
+                        Imprimante jet d'encre multifonctions HP DeskJet 2721e - WiFi + 6 mois de forfait d'impression Instant Ink
+                    </div>
+                    <div class="basket__item__price">
+                        49.99€
+                    </div>
+                </div>
+
+
+                <div class="basket__item__quantity">
+                    1
+                </div>
+
+                <div class="basket__item__total">
+
+                </div>
+            </div>
+        </div>
+
+        <div class="basket__item">
+
+            <img src="03.jpg" alt="">
+
+            <div class="basket__item__right">
+                <div class="basket__item__right__top">
+                    <div class="basket__item__text">
+                        PC Portable 15.6" Lenovo IdeaPad 3 15ALC6 - Full HD, Ryzen 3 5300U, RAM 8 Go, SSD 128 Go, Windows 11
+                    </div>
+                    <div class="basket__item__price">
+                        424.99€
+                    </div>
+                </div>
+
+
+                <div class="basket__item__quantity">
+                    1
+                </div>
+
+                <div class="basket__item__total">
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
